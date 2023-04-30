8998a12987ca34141d3471a8948b4c83f9f5dc62
James New <jnew@gulfcoastdesignz.com>
2023-04-30 16:54:50 -0500

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const [inputPasscode, setInputPasscode] = useState('');
 
   const passcode = {
-    id: 'HappiestCoupleInTheWorld',
+    id: 'happyfamily',
   };
 
   useEffect(() => {
